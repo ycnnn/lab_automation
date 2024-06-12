e4a91ee22ddc25f8a782b21507a952152a393a5c
Yue Zhang <yuez11@illinois.edu>
2024-06-11 21:10:21 -0500

    .

--- LaserScanningMicroscopy/ng_gui/params/scan_params.py
+++ LaserScanningMicroscopy/ng_gui/params/scan_params.py
@@ -4,10 +4,10 @@ class Scan_parameters:
     def __init__(self,
                  input_mapping=['ai0','ai1','ai2'],
                  frequency=1,
-                 channel_num=3,
+                 channel_num=None,
                  DAQ_name='Dev2'):
        
         self.frequency = frequency
-        self.channel_num = channel_num
+        self.channel_num = len(input_mapping) if not channel_num else channel_num
         self.input_mapping = input_mapping
         self.DAQ_name = DAQ_name
