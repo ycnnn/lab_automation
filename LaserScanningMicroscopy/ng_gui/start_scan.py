74b5bcc2fc1f096023ab0197b5671c6fea7f9ec1
Yue Zhang <yuez11@illinois.edu>
2024-06-11 21:15:10 -0500

    .

--- LaserScanningMicroscopy/ng_gui/start_scan.py
+++ LaserScanningMicroscopy/ng_gui/start_scan.py
@@ -7,6 +7,7 @@ from mp import Data_fetcher, Data_receiver
 from params.position_params import Position_parameters
 from params.scan_params import Scan_parameters
 from params.display_params import Display_parameters
+from main import lsm_scan
 ######################################################################
 
 
@@ -16,14 +17,13 @@ if __name__ == '__main__':
     position_parameters = Position_parameters(
                                             x_size=15,
                                             y_size=15,
-                                            x_pixels=256,
-                                            y_pixels=256,
+                                            x_pixels=128,
+                                            y_pixels=128,
                                             x_origin=10,
                                             y_origin=10,
                                             z_height=19)
     
-    scan_parameters = Scan_parameters(frequency=10, 
-                                      channel_num=3, 
+    scan_parameters = Scan_parameters(frequency=0.5, 
                                       input_mapping=["ai0","ai1","ai3"])
     
     display_parameters = Display_parameters(
@@ -37,19 +37,6 @@ if __name__ == '__main__':
                  darkmode=True,
                  save_data=True)
 
-    mp.freeze_support()
-    out_pipe, in_pipe = mp.Pipe(duplex=True)
-    data_fetcher = Data_fetcher(position_parameters=position_parameters,
-                                scan_parameters=scan_parameters, 
-                                pipe=out_pipe)
-    data_receiver = Data_receiver(position_parameters=position_parameters,
-                                  scan_parameters=scan_parameters, 
-                                  display_parameters=display_parameters,
-                                  pipe=in_pipe)
-    data_fetcher.start()
-    data_receiver.start()
-    data_fetcher.join()
-    data_receiver.join()
-    
-
-   
+    lsm_scan(position_parameters=position_parameters,
+             scan_parameters=scan_parameters,
+             display_parameters=display_parameters)
