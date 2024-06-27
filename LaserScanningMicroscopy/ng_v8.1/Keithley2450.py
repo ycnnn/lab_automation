eaba50a1fc0a0c82a9b89ddcb2caa66e07f737de
Yue Zhang <yuez11@illinois.edu>
2024-06-27 16:24:32 -0500

    .

---
+++ LaserScanningMicroscopy/ng_v8.1/Keithley2450.py
@@ -0,0 +1,384 @@
+import numpy as np
+import pyvisa as pv
+
+def set_smu_ready(address):
+    rm = pv.ResourceManager()
+    smu = rm.open_resource(address)
+    smu.timeout = 500
+    smu.write('reset()')
+    return smu
+
+def write_script(gate,
+                 gate_sweep_volt_list,
+                 drain_sweep_volt_list,
+                 gate_sweep_mode_list,
+                 drain_sweep_mode_list,
+                 delay = 0.01,
+                 script_name='sweepScript'):
+    ########################################
+    # Input check
+    if gate_sweep_volt_list.shape != gate_sweep_mode_list.shape:
+        print('Error: input volage and/or sweep mode list shape incorrect. ')
+        return
+    if drain_sweep_volt_list.shape != drain_sweep_mode_list.shape:
+        print('Error: input volage and/or sweep mode list shape incorrect. ')
+        return
+    if gate_sweep_mode_list.shape != drain_sweep_volt_list.shape:
+        print('Error: input volage and/or sweep mode list shape incorrect. ')
+        return
+    if len(gate_sweep_volt_list.shape) != 1:
+        print('Error: input volage and/or sweep mode list shape incorrect. ')
+        return
+    if np.max(np.abs(gate_sweep_volt_list)) >= 200 or np.max(np.abs(drain_sweep_volt_list)) >= 200:
+        print('Error: input volage too high.')
+        return
+    
+
+    gate.write('loadscript ' +  script_name)
+
+
+    gate.write("tsplink.initialize()")
+    gate.write("state = tsplink.state")
+    gate.write("if state ~= \"online\" then")
+    gate.write(" print(\"Error: Check that all SMUs have a different node number; and\")")
+    gate.write(" print(\"Check that all SMUs are connected correctly\")")
+    gate.write(" return")
+    gate.write("end")
+    gate.write("reset()")
+    ######################## Model 2450 #1 (gate) setup ################
+    step_points = len(gate_sweep_volt_list)
+    gate.write(f"steppoints = {step_points}")
+    #  Set up the source function.
+    gate.write("smu.source.configlist.create(\"gateSourceVals\")")
+    gate.write("smu.measure.configlist.create(\"gateMeasureVals\")")
+    gate.write("smu.source.func = smu.FUNC_DC_VOLTAGE")
+    gate.write("smu.source.autorange = smu.ON")
+    #  Set up the measure function.
+
+    #  Change me later
+    #  smu.measure.func = smu.FUNC_DC_CURRENT
+    gate.write("smu.measure.func = smu.FUNC_DC_VOLTAGE")
+    gate.write("smu.measure.autorange = smu.ON")
+    gate.write("smu.measure.terminals = smu.TERMINALS_FRONT")
+    #  Set up TSP-Link triggering.
+    gate.write("tsplink.line[1].reset()")
+    gate.write("tsplink.line[1].mode = tsplink.MODE_SYNCHRONOUS_MASTER")
+    gate.write("tsplink.line[2].mode = tsplink.MODE_SYNCHRONOUS_ACCEPTOR")
+    gate.write("trigger.tsplinkout[1].stimulus = trigger.EVENT_NOTIFY1")
+    gate.write("trigger.tsplinkin[2].clear()")
+    gate.write("trigger.tsplinkin[2].edge = trigger.EDGE_RISING")
+
+    # gate.write("for i = 1, 100 do")
+    for index in np.arange(step_points):
+        gate.write(f"smu.source.level = {gate_sweep_volt_list[index]}")
+        gate.write("smu.source.configlist.store(\"gateSourceVals\")")
+        if gate_sweep_mode_list[index] == 1:
+            gate.write("smu.measure.func = smu.FUNC_DC_CURRENT")
+        else:
+            gate.write("smu.measure.func = smu.FUNC_DC_VOLTAGE")
+        gate.write("smu.measure.configlist.store(\"gateMeasureVals\")")
+    # gate.write("end")
+    #  Set up the trigger model.
+    gate.write("trigger.model.setblock(1, trigger.BLOCK_CONFIG_RECALL, \"gateSourceVals\", 1, \"gateMeasureVals\", 1)")
+    gate.write("trigger.model.setblock(2, trigger.BLOCK_SOURCE_OUTPUT, smu.ON)")
+    gate.write("trigger.model.setblock(3, trigger.BLOCK_MEASURE_DIGITIZE)")
+    gate.write("trigger.model.setblock(4, trigger.BLOCK_NOTIFY, trigger.EVENT_NOTIFY1)")
+    gate.write("trigger.model.setblock(5, trigger.BLOCK_WAIT, trigger.EVENT_TSPLINK2)")
+    gate.write("trigger.model.setblock(6, trigger.BLOCK_CONFIG_NEXT, \"gateSourceVals\", \"gateMeasureVals\")")
+    gate.write("trigger.model.setblock(7, trigger.BLOCK_BRANCH_COUNTER, steppoints, 3)")
+    gate.write("trigger.model.setblock(8, trigger.BLOCK_SOURCE_OUTPUT, smu.OFF)")
+
+    #  ########################## Model 2450 #2 (drain) setup ################
+    gate.write("sweeppoints = steppoints")
+    #  Set up the source function.
+    gate.write("node[2].smu.source.configlist.create(\"drainSourceVals\")")
+    gate.write("node[2].smu.measure.configlist.create(\"drainMeasureVals\")")
+    gate.write("node[2].smu.source.func = node[2].smu.FUNC_DC_VOLTAGE")
+    gate.write("node[2].smu.source.autorange = node[2].smu.ON")
+    gate.write("node[2].smu.source.ilimit.level = 100e-3")
+    #  Set up the measure function.
+
+
+    #  Change me later
+
+    gate.write("node[2].smu.measure.func = node[2].smu.FUNC_DC_VOLTAGE")
+    gate.write("node[2].smu.measure.autorange = node[2].smu.ON")
+    gate.write("node[2].smu.measure.terminals = node[2].smu.TERMINALS_FRONT")
+
+    #  Set up TSP-Link triggering.
+    gate.write("node[2].tsplink.line[2].mode = node[2].tsplink.MODE_SYNCHRONOUS_MASTER")
+    gate.write("node[2].tsplink.line[1].mode = node[2].tsplink.MODE_SYNCHRONOUS_ACCEPTOR")
+    gate.write("node[2].trigger.tsplinkout[2].stimulus = node[2].trigger.EVENT_NOTIFY2")
+    gate.write("node[2].trigger.tsplinkin[1].clear()")
+    gate.write("node[2].trigger.tsplinkin[1].edge = node[2].trigger.EDGE_RISING")
+    #  Populate the drainSourceVals source config list, with source levels
+
+    for index in np.arange(len(drain_sweep_volt_list)):
+        gate.write(f"node[2].smu.source.level = {drain_sweep_volt_list[index]}")
+        gate.write("node[2].smu.source.configlist.store(\"drainSourceVals\")")
+        if drain_sweep_mode_list[index] == 1:
+            gate.write("node[2].smu.measure.func = node[2].smu.FUNC_DC_CURRENT")
+            gate.write("node[2].smu.source.autorange = node[2].smu.ON")
+        else:
+            gate.write("node[2].smu.measure.func = node[2].smu.FUNC_DC_VOLTAGE")
+            gate.write("node[2].smu.source.autorange = node[2].smu.ON")
+        gate.write("node[2].smu.measure.configlist.store(\"drainMeasureVals\")")
+    
+    #  Set up the trigger model.
+    gate.write("node[2].trigger.model.setblock(1, node[2].trigger.BLOCK_CONFIG_RECALL, \"drainSourceVals\", 1, \"drainMeasureVals\", 1)")
+    gate.write("node[2].trigger.model.setblock(2, node[2].trigger.BLOCK_SOURCE_OUTPUT, node[2].smu.ON)")
+    gate.write("node[2].trigger.model.setblock(3, node[2].trigger.BLOCK_WAIT, node[2].trigger.EVENT_TSPLINK1)")
+    gate.write(f"node[2].trigger.model.setblock(4, node[2].trigger.BLOCK_DELAY_CONSTANT, {delay})")
+    gate.write("node[2].trigger.model.setblock(5, node[2].trigger.BLOCK_MEASURE_DIGITIZE)")
+    #  new ltrigger flow
+    gate.write("node[2].trigger.model.setblock(6, node[2].trigger.BLOCK_NOTIFY, node[2].trigger.EVENT_NOTIFY2)")
+    gate.write("node[2].trigger.model.setblock(7, node[2].trigger.BLOCK_CONFIG_NEXT, \"drainSourceVals\", \"drainMeasureVals\")")
+    gate.write("node[2].trigger.model.setblock(8, node[2].trigger.BLOCK_BRANCH_COUNTER, sweeppoints, 3)")
+    gate.write("node[2].trigger.model.setblock(9, node[2].trigger.BLOCK_SOURCE_OUTPUT, node[2].smu.OFF)")
+
+
+    #  Start the trigger model for both SMUs and wait until it is complete
+    gate.write("node[2].trigger.model.initiate()")
+    gate.write("trigger.model.initiate()")
+    gate.write("waitcomplete()")
+    gate.write('endscript')
+    gate.write(script_name + '.save()')
+
+def run_script(gate, script_name):
+    gate.write(script_name + '.run()')
+    gate.write('script.delete(\"' + script_name +'\")')
+    # gate.write('waitcomplete()')
+
+def transfer_paramater_generator(
+                                 gate_start,
+                                 gate_end,
+                                 drain_voltage,
+                                 sweep_steps,
+                                 test_hysteresis,
+                                 test_leakage,
+                                 ramp_steps,
+                                 delay):
+    if test_hysteresis:
+        gate_sweep_volt_list = np.concatenate((
+            np.linspace(0, gate_start, num=ramp_steps),
+            np.linspace(gate_start, gate_end, num=sweep_steps),
+            np.linspace(gate_end, gate_start, num=sweep_steps),
+            np.linspace(gate_start, 0, num=ramp_steps)
+        ))
+        drain_sweep_volt_list = np.concatenate((
+            np.linspace(0, drain_voltage, num=ramp_steps),
+            np.linspace(drain_voltage, drain_voltage, num=sweep_steps),
+            np.linspace(drain_voltage, drain_voltage, num=sweep_steps),
+            np.linspace(drain_voltage, 0, num=ramp_steps)
+        ))
+    else:
+        gate_sweep_volt_list = np.concatenate((
+            np.linspace(0, gate_start, num=ramp_steps),
+            np.linspace(gate_start, gate_end, num=sweep_steps),
+            np.linspace(gate_end, 0, num=ramp_steps)
+        ))
+        drain_sweep_volt_list = np.concatenate((
+            np.linspace(0, drain_voltage, num=ramp_steps),
+            np.linspace(drain_voltage, drain_voltage, num=sweep_steps),
+            np.linspace(drain_voltage, 0, num=ramp_steps)
+        ))
+
+    leakage_indicator = 1 if test_leakage else 0
+
+    gate_sweep_mode_list = np.concatenate((
+        np.zeros(ramp_steps),
+        np.ones(int(len(drain_sweep_volt_list) - 2 * ramp_steps)) * leakage_indicator,
+        np.zeros(ramp_steps)
+    ))
+    drain_sweep_mode_list = np.concatenate((
+        np.zeros(ramp_steps),
+        np.ones(int(len(drain_sweep_volt_list) - 2 * ramp_steps)),
+        np.zeros(ramp_steps)
+    ))
+
+    script_name = 'sweepScript'
+
+    parameters = {
+                  'script_name':script_name,
+                  'gate_sweep_volt_list':gate_sweep_volt_list,
+                  'drain_sweep_volt_list':drain_sweep_volt_list,
+                  'gate_sweep_mode_list':gate_sweep_mode_list,
+                  'drain_sweep_mode_list':drain_sweep_mode_list,
+                  'delay':delay}
+    return parameters
+
+def output_paramater_generator(
+                                 drain_start,
+                                 drain_end,
+                                 gate_voltage,
+                                 sweep_steps,
+                                 test_hysteresis,
+                                 ramp_steps,
+                                 delay):
+    if test_hysteresis:
+        gate_sweep_volt_list = np.concatenate((
+            np.linspace(0, gate_voltage, num=ramp_steps),
+            np.linspace(gate_voltage, gate_voltage, num=sweep_steps),
+            np.linspace(gate_voltage, gate_voltage, num=sweep_steps),
+            np.linspace(gate_voltage, 0, num=ramp_steps)
+        ))
+        drain_sweep_volt_list = np.concatenate((
+            np.linspace(0, drain_start, num=ramp_steps),
+            np.linspace(drain_start, drain_end, num=sweep_steps),
+            np.linspace(drain_end, drain_start, num=sweep_steps),
+            np.linspace(drain_start, 0, num=ramp_steps)
+        ))
+    else:
+        gate_sweep_volt_list = np.concatenate((
+            np.linspace(0, gate_voltage, num=ramp_steps),
+            np.linspace(gate_voltage, gate_voltage, num=sweep_steps),
+            np.linspace(gate_voltage, 0, num=ramp_steps)
+        ))
+        drain_sweep_volt_list = np.concatenate((
+            np.linspace(0, drain_start, num=ramp_steps),
+            np.linspace(drain_start, drain_end, num=sweep_steps),
+            np.linspace(drain_end, 0, num=ramp_steps)
+        ))
+
+    gate_sweep_mode_list = np.concatenate((
+        np.zeros(ramp_steps),
+        np.zeros(int(len(drain_sweep_volt_list) - 2 * ramp_steps)),
+        np.zeros(ramp_steps)
+    ))
+    drain_sweep_mode_list = np.concatenate((
+        np.zeros(ramp_steps),
+        np.ones(int(len(drain_sweep_volt_list) - 2 * ramp_steps)),
+        np.zeros(ramp_steps)
+    ))
+
+    script_name = 'sweepScript'
+    parameters = {
+                  'script_name':script_name,
+                  'gate_sweep_volt_list':gate_sweep_volt_list,
+                  'drain_sweep_volt_list':drain_sweep_volt_list,
+                  'gate_sweep_mode_list':gate_sweep_mode_list,
+                  'drain_sweep_mode_list':drain_sweep_mode_list,
+                  'delay':delay}
+    return parameters
+
+def fetch_readings(gate, timeout):
+    for _ in range(int(timeout/(gate.timeout/1000))):
+        try:
+            status = gate.query('print(trigger.model.state())')
+            break
+        except:
+            print('Sweep started >>>>>>>>', end = '\r')
+    gate.clear()
+    print('Sweep completed >>>>>>', end='\r')
+    gate_query_data = gate.query_ascii_values('printbuffer(1, defbuffer1.n, defbuffer1.sourcevalues, defbuffer1.readings)')
+    gate_readings = np.array(gate_query_data).reshape(-1,2)
+    drain_query_data = gate.query_ascii_values('printbuffer(1, node[2].defbuffer1.n, node[2].defbuffer1.sourcevalues, node[2].defbuffer1.readings)')
+    drain_readings = np.array(drain_query_data).reshape(-1,2)
+    return gate_readings, drain_readings
+
+def transfer(
+            gate_start,
+            gate_end,
+            drain_voltage,
+            sweep_steps,
+            test_hysteresis,
+            test_leakage,
+            ramp_steps=20,
+            delay=0.01,
+            gate_address="USB0::0x05E6::0x2450::04096331::INSTR",
+            drain_address="USB0::0x05E6::0x2450::04096333::INSTR"):
+    
+    transfer_params = transfer_paramater_generator(gate_start=gate_start, 
+                             gate_end=gate_end,
+                             drain_voltage=drain_voltage,
+                             ramp_steps=ramp_steps,
+                             sweep_steps=sweep_steps,
+                             test_hysteresis=test_hysteresis,
+                             test_leakage=test_leakage,
+                             delay=delay)
+    gate = set_smu_ready(gate_address)
+    _ = set_smu_ready(drain_address)
+    write_script(gate=gate,**transfer_params)
+    gate.write(transfer_params['script_name'] + '.run()')
+    gate.write('script.delete(\"' + transfer_params['script_name'] +'\")')
+
+    gate_readings, drain_readings = fetch_readings(gate, timeout=36000)
+    _ = set_smu_ready(gate_address)
+    _ = set_smu_ready(drain_address)
+
+    return gate_readings, drain_readings
+
+def output(
+            drain_start,
+            drain_end,
+            gate_voltage,
+            sweep_steps,
+            test_hysteresis,
+            ramp_steps=20,
+            delay=0.01,
+            gate_address="USB0::0x05E6::0x2450::04096331::INSTR",
+            drain_address="USB0::0x05E6::0x2450::04096333::INSTR"):
+    
+    output_params = output_paramater_generator(
+                                 drain_start=drain_start,
+                                 drain_end=drain_end,
+                                 gate_voltage=gate_voltage,
+                                 sweep_steps=sweep_steps,
+                                 test_hysteresis=test_hysteresis,
+                                 ramp_steps=ramp_steps,
+                                 delay=delay)
+
+    gate = set_smu_ready(gate_address)
+    _ = set_smu_ready(drain_address)
+    write_script(gate=gate, **output_params)
+    gate.write(output_params['script_name'] + '.run()')
+    gate.write('script.delete(\"' + output_params['script_name'] +'\")')
+    gate_readings, drain_readings = fetch_readings(gate, timeout=36000)
+    _ = set_smu_ready(gate_address)
+    _ = set_smu_ready(drain_address)
+
+    return gate_readings[20:-20,:], drain_readings[20:-20,:]
+
+
+
+###########################################################################################################################
+# The following script is for ramping up single Keithley
+
+
+def set_smu_ready_for_ramp(address="USB0::0x05E6::0x2450::04096331::INSTR"):
+    rm = pv.ResourceManager()
+    smu = rm.open_resource(address)
+    smu.timeout = 500
+    smu.write('reset()')
+    smu.write("smu.source.autorange = smu.ON")
+    smu.write("smu.measure.func = smu.FUNC_DC_VOLTAGE")
+    smu.write("smu.measure.autorange = smu.ON")
+    smu.write("smu.measure.terminals = smu.TERMINALS_FRONT")
+    smu.write("smu.measure.func = smu.FUNC_DC_VOLTAGE")
+    smu.write("smu.source.level = 0")
+    return smu
+
+def ramp(smu,
+         start_volt=None, 
+         end_volt=0, 
+         ramp_steps=10):
+    # By default, ramp Keithley 2450 SMU from current voltage level to the target voltage level (end_volt).
+
+    smu.write('reading = smu.measure.read()')
+    volt_reading = smu.query_ascii_values('print(reading)')
+    # print(f'Current VOLT reading is {volt_reading} V.')
+    start_volt = start_volt if start_volt else volt_reading 
+
+
+    voltages = np.linspace(start_volt, end_volt, ramp_steps)
+    volt_readings = []
+    # return start_volt
+    for volt in voltages:
+        smu.write(f"smu.source.level = {volt}")
+        smu.write('reading = smu.measure.read()')
+        volt_reading = smu.query_ascii_values('print(reading)')
+        smu.write('waitcomplete()')
+        volt_readings.append(volt_reading)
+        # break
+
+    return np.array(volt_readings).reshape(-1)[-1]
---
+++ LaserScanningMicroscopy/ng_v8.1/inst_driver.py
@@ -0,0 +1,185 @@
+"""Simultaneous read and write with NI USB-4431 or similar device."""
+
+import numpy as np
+import pyvisa
+from contextlib import contextmanager
+import warnings
+
+instrument_props = {
+    'Lockin': {'additional_channel_num':2},
+    'Keithley2450': {'additional_channel_num':1},
+    'Empty_instrument': {'additional_channel_num':0},
+    'Virtual_instrument': {'additional_channel_num':1},
+}
+
+class External_instrument:
+    def __init__(self, 
+                 instrument_type=None,
+                 address="USB0::0xB506::0x2000::002765::INSTR", 
+                 **kwargs
+                 ) -> None:
+        self.address = address
+        self.instrument_type = instrument_type
+        self.additional_channel_num = 0
+        self.additional_channel_num = instrument_props[instrument_type]['additional_channel_num']
+        self.kwargs = kwargs
+
+def configurate_instrument(instrument,
+                          scan_parameters, 
+                          position_parameters,
+                          **kwargs):
+    if instrument.instrument_type == 'Lockin':
+        instr = Lockin(scan_parameters, 
+                    position_parameters,
+                    time_constant_level=5,
+                    **kwargs)
+                
+    elif instrument.instrument_type == 'Virtual_instrument':
+        instr = Virtual_instrument(
+                scan_parameters, 
+                position_parameters,
+                **kwargs)     
+
+    elif instrument.instrument_type == 'Keithley2450':
+        instr = Keithley2450(
+                scan_parameters, 
+                position_parameters,
+                **kwargs)     
+                      
+    else:
+        instr = Empty_instrument(
+                scan_parameters, 
+                position_parameters,)
+    return instr
+
+class Empty_instrument:
+    def __init__(self, scan_parameters=None, position_parameters=None):
+        self.initialize_instrument()
+        self.reading_num = position_parameters.x_pixels
+        self.data = np.empty((0, self.reading_num))
+    
+    @contextmanager
+    def initialize_instrument(self):
+        try:
+            pass
+            yield None
+        finally:
+            pass
+        
+    @contextmanager
+    def scan(self, **kwargs):
+        try:
+            pass
+            yield None
+        finally:
+            pass
+
+class Lockin:
+    def __init__(self, scan_parameters, position_parameters, 
+                #  time_constant_level=0,
+                 sample_count=128,
+                 **kwargs):
+
+        self.address = scan_parameters.instrument.address
+        
+        self.sample_count = sample_count
+        # self.additional_channel_num = 2
+        self.reading_num = position_parameters.x_pixels
+        # self.time_constant_level = time_constant_level
+        self.kwargs = kwargs
+        # self.initialize_instrument()
+ 
+    @contextmanager
+    def initialize_instrument(self):
+        try:
+            rm = pyvisa.ResourceManager()
+            self.instrument = rm.open_resource(self.address)
+
+            # Something happens here
+            if 'time_constant_level' in self.kwargs:
+                self.time_constant_level = self.kwargs['time_constant_level']
+            else:
+                warnings.warn('\n\n\nThe time constant of the lock-in is not provided.\nThe time constant has been set to the default value as 5.\n\n\n')
+                self.time_constant_level = 5
+
+            self.instrument.write('*rst')
+            # self.instrument.query('*idn?')
+            self.instrument.write(f'oflt {self.time_constant_level}')
+            self.instrument.write('capturelen 256')
+            self.instrument.write('capturecfg xy')
+            self.instrument.write('rtrg posttl')
+
+            yield self.instrument
+
+        finally:
+            self.instrument.close()
+        
+    @contextmanager
+    def scan(self, **kwargs):
+        try:
+            self.instrument.write('capturestart one, samp')
+            yield None
+        finally:
+            self.instrument.write('capturestop')
+            buffer_len = int(self.instrument.query('captureprog?')[:-1])
+            self.data = np.array(
+                self.instrument.query_binary_values(f'captureget? 0, {buffer_len}')
+                ).reshape(-1,2)[:self.reading_num,:].T
+
+class Virtual_instrument:
+    def __init__(self, scan_parameters=None, position_parameters=None):
+        self.initialize_instrument()
+        self.reading_num = position_parameters.x_pixels
+        
+    
+    @contextmanager
+    def initialize_instrument(self):
+        try:
+            pass
+            yield None
+        finally:
+            pass
+        
+    @contextmanager
+    def scan(self, **kwargs):
+        try:
+            pass
+            # self.data = np.empty((0, self.reading_num))
+            self.data = np.random.random(size=(1, self.reading_num))
+            yield None
+        finally:
+            pass
+
+class Keithley2450:
+    def __init__(self, scan_parameters=None, position_parameters=None, **kwargs):
+        self.initialize_instrument()
+        self.reading_num = position_parameters.x_pixels
+        self.kwargs = kwargs
+        # self.start_volt = self.kwargs['start_volt']
+        # self.end_volt = self.kwargs['end_volt']
+        # if 'val' in self.kwargs:
+        #     self.data = np.random.random(size=(1, self.reading_num)) * 0.1 + self.kwargs['val']
+        # else:
+        #     warnings.warn('\n\n\nThe VAL of the SMU is not provided.\nThe VAL has been set to the default value as 0.\n\n\n')
+        
+    
+    @contextmanager
+    def initialize_instrument(self):
+        try:
+            pass
+            yield None
+        finally:
+            pass
+        
+    @contextmanager
+    def scan(self, **kwargs):
+        try:
+            pass
+            if 'val' in self.kwargs:
+                self.data = np.random.random(size=(1, self.reading_num)) * 0.1 + self.kwargs['val'] + 0.1 * kwargs['scan_index']
+            else:
+                warnings.warn('\n\n\nThe VAL of the SMU is not provided.\nThe VAL has been set to the default value as 0.\n\n\n')
+                self.data = np.random.random(size=(1, self.reading_num)) * 0.1 + 0.1 * kwargs['scan_index']
+            yield None
+        finally:
+            pass
