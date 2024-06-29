2f5ca50ac7f9db8e5d23c49c5c76e1a1fe9f23cb
Yue Zhang <yuez11@illinois.edu>
2024-06-29 16:01:29 -0500

    .

--- LaserScanningMicroscopy/next_gen_software_v11/start_scan.py
+++ LaserScanningMicroscopy/next_gen_software_v11/start_scan.py
@@ -14,57 +14,59 @@ from inst_driver import External_instrument
 
 if __name__ == '__main__':
     
-
+    display_parameters = Display_parameters(scan_id='MOKE_Test_sample_high_resolution')
 
     position_parameters = Position_parameters(
-                                            x_size=30,
-                                            y_size=30,
-                                            x_pixels=128,
-                                            y_pixels=127,
-                                            z_center=19,
+                                            x_size=60,
+                                            y_size=60,
+                                            x_pixels=384,
+                                            # y_pixels=127,
+                                            z_center=25,
                                             angle=90)
     
-    scan_parameters = Scan_parameters(frequency=25, 
-                                      input_mapping=["ai0","ai1","ai2"],
-                                    #   instrument=instrument,
+    scan_parameters = Scan_parameters(frequency=1, 
+                                      input_mapping=["ai0","ai1"],
                                       return_to_zero=True)
-    # Setting up the external input instrument(s)
-    #############################################################################################
-    #############################################################################################
-    #############################################################################################
+    
     # Mandatory code. There MUST be at least one external instrument present dring the scan.
-    instrument = External_instrument(instrument_type='Empty_instrument')
-    scan_parameters.add_instrument(instrument)
+    empty_instr = External_instrument(instrument_type='Empty_instrument')
+    scan_parameters.add_instrument(empty_instr)
+
+    # Setting up other external input instrument(s)
+    # Uncomment as needed
     #############################################################################################
     #############################################################################################
     #############################################################################################
+    
+
+
+    
+
 
     # Setting up the external input instrument(s)
     # Sometimes, the code will ask for additional parameters for setting up the instrument.
     # Even if those parameters are not supplied, the scan will go on, but the system will use default values and issue warning(s).
     
-    Keithley_prop = {'start_volt': -2, 'end_volt': 2}
-    instrument2 = External_instrument(instrument_type='Keithley2450', **Keithley_prop)
-    scan_parameters.add_instrument(instrument2)
+    # Keithley_prop = {'start_volt': -2, 'end_volt': 2}
+    # instrument2 = External_instrument(instrument_type='Keithley2450', **Keithley_prop)
+    # scan_parameters.add_instrument(instrument2)
 
-    instrument3 = External_instrument(instrument_type='Virtual_instrument')
-    scan_parameters.add_instrument(instrument3)
+    # instrument3 = External_instrument(instrument_type='Virtual_instrument')
+    # scan_parameters.add_instrument(instrument3)
 
 
-    Lockin_prop = {'time_constant_level': 1, 'sample_count': 128}
-    instrument4 = External_instrument(instrument_type='Lockin', **Lockin_prop)
-    scan_parameters.add_instrument(instrument4)
+    # Lockin_prop = {'time_constant_level': 1, 'sample_count': 128}
+    # instrument4 = External_instrument(instrument_type='Lockin', **Lockin_prop)
+    # scan_parameters.add_instrument(instrument4)
+
     
-    display_parameters = Display_parameters(
-                 scan_id='Test',
-                 save_destination=None,
-                 colormap=None,
-                 channel_min=None,
-                 channel_max=None,
-                 window_width=None,
-                 window_height=None,
-                 darkmode=True,
-                 save_data=True)
+    #############################################################################################
+    #############################################################################################
+    #############################################################################################
+
+
+    
+    
     
     # scan_parameters.save_params(display_parameters.save_destination + display_parameters.scan_id)
     # position_parameters.save_params(display_parameters.save_destination + display_parameters.scan_id)
