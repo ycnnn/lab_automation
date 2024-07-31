da0c88f44fb00f2ca70af65338afc4f608890f52
Yue Zhang <yuez11@illinois.edu>
2024-07-31 09:04:49 -0500

    .

--- LaserScanningMicroscopy/next_gen_software_v14/params/display_params.py
+++ LaserScanningMicroscopy/next_gen_software_v14/params/display_params.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os
+import shutil
 from pathlib import Path
 
 class Display_parameters:
@@ -26,9 +27,25 @@ class Display_parameters:
         path = str(Path(os.path.dirname(full_path)).parent.absolute()) + '/results/' + self.scan_id + '/'
         self.save_destination = path if not save_destination else save_destination
 
+        # The following code save the data.
+        # In case there is already a filder that hav the same name as the save desitnation,
+        # The code will rename the old folder by adding a suffix of "_backup".
+        # In cases there is alreay a backup folder, the code will add a number to suffix to identify each backup.
+        # For example: if "test" alreay exist, the code will try to move the old data to "text_backup_1"; if "text_backup_1" is also there, the code will try change the identifier to "text_backup_2", "text_backup_3", "text_backup_4" etc.
+        # In short, the code NEVER overwrites nor DELETES your data!
+
         if not os.path.exists(self.save_destination):
             # If the directory does not exist, create it
             os.makedirs(self.save_destination)
+
+        else:
+            uniq = 1
+            backup_path = path[:-1] + '_backup_' + str(uniq) + '/'
+            while os.path.exists(backup_path):
+                uniq += 1
+                backup_path = path[:-1] + '_backup_' + str(uniq) + '/'
+            shutil.copytree(self.save_destination, backup_path)
+            pass
 
         self.colormap = colormap
         self.channel_min = channel_min
