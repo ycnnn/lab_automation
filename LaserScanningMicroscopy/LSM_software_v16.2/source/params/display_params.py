8951770024d58563338973b978c26fedf04513a0
Yue Zhang <yuez11@illinois.edu>
2024-08-04 14:48:27 -0500

    .

---
+++ LaserScanningMicroscopy/LSM_software_v16.2/source/params/display_params.py
@@ -0,0 +1,74 @@
+import numpy as np
+import os
+import shutil
+from pathlib import Path
+
+class Display_parameters:
+    def __init__(self,
+                 scan_id=None,
+                 save_destination=None,
+                 colormap=None,
+                 channel_min=None,
+                 channel_max=None,
+                 window_width=None,
+                 window_height=None,
+                 darkmode=True,
+                 text_bar_height=20,
+                 window_width_min=300, 
+                 window_width_max=500,
+                 show_zero_level=True,
+                 save_data=True):
+        
+        if len(scan_id) == 0:
+            scan_id = 'temp_scan'
+        
+        self.scan_id = 'temp_scan' if not scan_id else scan_id
+    
+        
+        full_path = os.path.realpath(__file__)
+        path = str(Path(os.path.dirname(full_path)).parents[1].absolute()) + '/results/' + self.scan_id + '/'
+    
+        self.save_destination = path if not save_destination else save_destination
+
+        # The following code save the data.
+        # In case there is already a filder that hav the same name as the save desitnation,
+        # The code will rename the old folder by adding a suffix of "_backup".
+        # In cases there is alreay a backup folder, the code will add a number to suffix to identify each backup.
+        # For example: if "test" alreay exist, the code will try to move the old data to "text_backup_1"; if "text_backup_1" is also there, the code will try change the identifier to "text_backup_2", "text_backup_3", "text_backup_4" etc.
+        # In short, the code NEVER overwrites nor DELETES your data!
+
+        if not os.path.exists(self.save_destination):
+            # If the directory does not exist, create it
+            os.makedirs(self.save_destination)
+
+        else:
+            uniq = 1
+            backup_path = path[:-1] + '_backup_' + str(uniq) + '/'
+            while os.path.exists(backup_path):
+                uniq += 1
+                backup_path = path[:-1] + '_backup_' + str(uniq) + '/'
+            # shutil.copytree(self.save_destination, backup_path)
+            # shutil.rmtree(self.save_destination)
+            # os.makedirs(self.save_destination)
+            os.rename(self.save_destination, backup_path)
+        
+
+        os.makedirs(self.save_destination + 'data/')
+        os.makedirs(self.save_destination + 'data_text/')
+        os.makedirs(self.save_destination + 'parameters/')
+
+        self.colormap = colormap
+        self.channel_min = channel_min
+        self.channel_max = channel_max
+        self.window_width = window_width
+        self.window_height = window_height
+        self.text_bar_height = text_bar_height
+        self.window_width_min = window_width_min
+        self.window_width_max = window_width_max
+        self.darkmode = darkmode
+        self.save_data = save_data
+        self.show_zero_level = show_zero_level
+
+
+       
+        
---
+++ LaserScanningMicroscopy/LSM_software_v16.2/utilities/set_z_height.py
@@ -0,0 +1,54 @@
+import os
+import itertools
+parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+os.sys.path.insert(0,parentdir) 
+from source.params.position_params import Position_parameters
+from source.params.scan_params import Scan_parameters
+# from source.daq_driver_simulated import reset_daq, set_z_height, read_daq_output
+from source.daq_driver import reset_daq, set_z_height, read_daq_output
+
+if __name__=='__main__':
+    message = 'Enter command.\nEnter a number to move the stage height;\nEnter R for resetting the DAQ;\nPress enter to switch the stage height between the last two values you entered.\nEnter anything else to quit the code.\n\n'
+
+    command = input(message).capitalize()
+    scan_parameters = Scan_parameters()
+    heights = [0,0]
+    alter_index = 1
+
+    while True:
+
+        try:
+            z_height = float(command)
+            position_parameters = Position_parameters(z_center=float(z_height))
+            set_z_height(scan_parameters=scan_parameters,
+                                    position_parameters=position_parameters)
+            daq_output = read_daq_output(scan_parameters=scan_parameters)
+            print('DAQ output has been set to: ')
+            print(daq_output)
+            print('\n')
+            heights.pop(0)
+            heights.append(z_height)
+        except:
+            if command == 'R':
+                reset_daq(scan_parameters=scan_parameters)
+                daq_output = read_daq_output(scan_parameters=scan_parameters)
+                print('DAQ output has been reset to: ')
+                print(daq_output)
+                print('\n')
+            elif len(command) == 0:
+                print('Start alternating between the last two Z heights you entered. To switch, press Enter. To quit, press Q.\n')
+                while True:
+                    # Move
+                    new_command = input('Switch the height by press Enter, press any other key to quit\n')
+                    alter_index += 1
+                    if new_command != "":
+                        break
+                    z_height = heights[int(alter_index % 2)]
+                    position_parameters = Position_parameters(z_center=float(z_height))
+                    set_z_height(scan_parameters=scan_parameters,
+                                            position_parameters=position_parameters)
+            else:
+                break
+        command = input(message).capitalize()
+
+                    
