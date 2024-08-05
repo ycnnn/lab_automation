fe916b74e2bc0e573515894a4be41e1526fedf51
Yue Zhang <yuez11@illinois.edu>
2024-08-05 14:58:43 -0500

    Update set_and_monitor_K10CR1.py

--- LaserScanningMicroscopy/LSM_software_v16.3a/utilities/set_and_monitor_K10CR1.py
+++ LaserScanningMicroscopy/LSM_software_v16.3a/utilities/set_and_monitor_K10CR1.py
@@ -1,3 +1,7 @@
+import os
+parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+os.sys.path.insert(0,parentdir) 
+
 from external_instrument_drivers.K10CR1 import K10CR1_stage
 
 if __name__ == '__main__':
