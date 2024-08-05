2c0167682224d10dd8ff02e1ab5d3938c5892702
Yue Zhang <yuez11@illinois.edu>
2024-08-05 13:46:18 -0500

    .

--- LaserScanningMicroscopy/LSM_software_v16.3a/pre_made_scan_templates/imaging_with_gate_bias.py
+++ LaserScanningMicroscopy/LSM_software_v16.3a/pre_made_scan_templates/imaging_with_gate_bias.py
@@ -34,16 +34,16 @@ if __name__ == '__main__':
                                             z_center=0,
                                             angle=-35)
     
-    scan_parameters = Scan_parameters(point_time_constant=0.0001,
-                                    #   retrace_point_time_constant=0.00002,
+    scan_parameters = Scan_parameters(point_time_constant=0.11,
+                                      retrace_point_time_constant=0.02,
                                       input_mapping=["ai0"],
                                       return_to_zero=True)
 
 
   
-    # Laser_prop = {'current_level': 0.010}
-    # instrument3 = External_instrument(instrument_type='Laser', **Laser_prop)
-    # scan_parameters.add_instrument(instrument3)
+    Laser_prop = {'current_level': 0.080}
+    instrument3 = External_instrument(instrument_type='Laser', **Laser_prop)
+    scan_parameters.add_instrument(instrument3)
 
   
 
@@ -56,22 +56,24 @@ if __name__ == '__main__':
 
     # instrument2 = External_instrument(instrument_type='Virtual_instrument')
     # scan_parameters.add_instrument(instrument2)
+    
+    gate_bias = -40
 
-    # Keithley_prop = {'start_volt': -60, 'end_volt': -30}
-    # instrument3 = External_instrument(instrument_type='Keithley2450', **Keithley_prop)
-    # scan_parameters.add_instrument(instrument3)
+    Keithley_prop = {'start_volt': gate_bias, 'end_volt': gate_bias}
+    instrument3 = External_instrument(instrument_type='Keithley2450', **Keithley_prop)
+    scan_parameters.add_instrument(instrument3)
 
     # Levels and range: 0->1us, 1->3us, 2->10us 3->30us, 4->100us, 5->300us, 6->1ms, 7->3ms, 8->10ms, 
     # 9->30ms, 10->100ms, 11->300ms, 12->1s, 13->3s, 14->10s, 15->30s, 16->100s, 17->300s, 18->1000s, 19->3000s, 20->10000s
-    # Lockin_prop = {
-    #                 'time_constant_level':11, 
-    #                 'volt_input_range':1, 
-    #                 'signal_sensitivity':6,
-    #                 'ref_frequency':20170,
-    #                 'sine_amplitude':1.0
-    #                 }
-    # instrument4 = External_instrument(instrument_type='Lockin', **Lockin_prop)
-    # scan_parameters.add_instrument(instrument4)
+    Lockin_prop = {
+                    'time_constant_level':10, 
+                    'volt_input_range':1, 
+                    'signal_sensitivity':6,
+                    'ref_frequency':20170,
+                    'sine_amplitude':1.0
+                    }
+    instrument4 = External_instrument(instrument_type='Lockin', **Lockin_prop)
+    scan_parameters.add_instrument(instrument4)
 
 
     
