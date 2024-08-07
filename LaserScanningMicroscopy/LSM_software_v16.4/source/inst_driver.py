8239cc28a6f09ed21a247f00f1b7fcff67ef8421
Yue Zhang <yuez11@illinois.edu>
2024-08-07 16:28:21 -0500

    .

--- LaserScanningMicroscopy/LSM_software_v16.4/source/inst_driver.py
+++ LaserScanningMicroscopy/LSM_software_v16.4/source/inst_driver.py
@@ -341,7 +341,7 @@ class LaserDiode:
             rm = pyvisa.ResourceManager()
             self.instrument = rm.open_resource(self.instrument_params['address'])
             # self.instrument.write('*rst')
-            if self.instrument_params['current_level'] < 0 or self.instrument_params['current_level'] >= 0.101:
+            if np.min(self.current_levels) < 0 or np.max(self.current_levels) >= 0.101:
                 print('Warning: the laser current setpoint is outside the allowed range.\nFor your safety, the laser power has been set to 10 mA.')
                 self.instrument_params['current_level'] = 0.01
             self.instrument.write(f"source1:current:level:amplitude {self.instrument_params['current_level']}")
