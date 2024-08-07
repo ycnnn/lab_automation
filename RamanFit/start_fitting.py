7d7b033e249f597b43d8353f9cbe40563cb77d57
Yue Zhang <yuez11@illinois.edu>
2024-08-07 13:08:24 -0500

    .

--- RamanFit/start_fitting.py
+++ RamanFit/start_fitting.py
@@ -21,43 +21,36 @@ def start_MoS2_fit(filename,
     return si_params, params
 
 if __name__ == "__main__":
+
+    filename = sys.argv[1].split('.txt') + '_'
     
-    if len(sys.argv) > 2:
-        print('Enter the customization.') 
-        peak_0_center_initial_guess = float(input('Initial E peak position guess (in cm-1) ? \n'))
-        peak_1_center_initial_guess = float(input('Initial A peak position guess (in cm-1) ? \n'))
-        save_format = float(input('How to save the result? Enter 0 for .csv file, enter 1 for .npy file. \n'))
-
-        filename = sys.argv[1].split('.txt') + '_'
-        si_params, params = start_MoS2_fit(sys.argv[1],
-                        peak_0_center_initial_guess=peak_0_center_initial_guess,
-                        peak_1_center_initial_guess=peak_1_center_initial_guess)
-        if save_format == 0:
-            np.savetxt(filename + 'Si_peak_position.csv', si_params['peak_0_centers'])
-            np.savetxt(filename + 'E_peak_position.csv', params['peak_0_centers'])
-            np.savetxt(filename + 'A_peak_position.csv', params['peak_1_centers'])
-        else:
-            np.save(filename + 'Si_peak_position', si_params['peak_0_centers'])
-            np.save(filename + 'E_peak_position', params['peak_0_centers'])
-            np.save(filename + 'A_peak_position', params['peak_1_centers'])
-    else:
-        si_params, params = start_MoS2_fit(sys.argv[1])
-        np.savetxt(filename + 'Si_peak_position.csv', si_params['peak_0_centers'])
-        np.savetxt(filename + 'E_peak_position.csv', params['peak_0_centers'])
-        np.savetxt(filename + 'A_peak_position.csv', params['peak_1_centers'])
-        
-  
-
-
-    # if len(sys.argv) == 4:
-    #     peak_0_center_initial_guess = float(sys.argv[2])
-    #     peak_1_center_initial_guess = float(sys.argv[3])
-    #     si_params, params = start_MoS2_fit(sys.argv[1],
-    #                    peak_0_center_initial_guess=peak_0_center_initial_guess,
-    #                    peak_1_center_initial_guess=peak_1_center_initial_guess)
-    # else:
-    #     si_params, params = start_MoS2_fit(sys.argv[1])
+ 
+    peak_0_center_initial_guess = 384.0
+    peak_1_center_initial_guess = 404.0
     
-    # np.savetxt(si_params['peak_0_centers'])
-    # np.savetxt(params['peak_1_centers'])
-    # np.savetxt(params['peak_1_centers'])
+    
+    si_params, params = start_MoS2_fit(sys.argv[1],
+                    peak_0_center_initial_guess=peak_0_center_initial_guess,
+                    peak_1_center_initial_guess=peak_1_center_initial_guess)
+    
+    correction = si_params['peak_0_centers'] - 520.0
+    E_pos_corrected = params['peak_0_centers'] - correction
+    A_pos_corrected = params['peak_1_centers'] - correction
+    strain = -0.20 * E_pos_corrected + 0.03 * A_pos_corrected
+    doping = -0.15 * E_pos_corrected + 0.47 * A_pos_corrected
+
+ 
+    np.savetxt(filename + 'Si_raw_peak_position.csv', si_params['peak_0_centers'])
+    np.savetxt(filename + 'E_raw_peak_position.csv', params['peak_0_centers'])
+    np.savetxt(filename + 'A_raw_peak_position.csv', params['peak_1_centers'])
+    np.savetxt(filename + 'strain.csv', strain)
+    np.savetxt(filename + 'doping.csv', doping)
+
+    np.save(filename + 'Si_raw_peak_position', si_params['peak_0_centers'])
+    np.save(filename + 'E_raw_peak_position', params['peak_0_centers'])
+    np.save(filename + 'A_raw_peak_position', params['peak_1_centers'])
+    np.save(filename + 'strain', strain)
+    np.save(filename + 'doping', doping)
+
+
+
