6dcfdfecee79e4ffbfd3f4491eaa346fb1b12679
Yue Zhang <yuez11@illinois.edu>
2024-09-02 11:15:33 -0500

    .

--- LaserScanningMicroscopy/LSM_software_v19/external_instrument_drivers/K10CR1.py
+++ LaserScanningMicroscopy/LSM_software_v19/external_instrument_drivers/K10CR1.py
@@ -9,6 +9,7 @@ import clr
 import os 
 import time 
 import sys
+import pyvisa
 
 # Write in file paths of dlls needed. 
 clr.AddReference("C:\\Program Files\\Thorlabs\\Kinesis\\Thorlabs.MotionControl.DeviceManagerCLI.dll")
@@ -21,10 +22,13 @@ from Thorlabs.MotionControl.GenericMotorCLI import *
 from Thorlabs.MotionControl.IntegratedStepperMotorsCLI import *
 from System import Decimal 
 
+
 class K10CR1_stage:
-    def __init__(self, serial_no=55425494) -> None:
-        # print('Important: make sure you are not running Kinesis software in the meantime. \nOtherwise the initialization will fail.')
+    def __init__(self, serial_no=55425494, verbose=True, velocity=20.0) -> None:
+        print('Important: make sure you are not running Kinesis software in the meantime. \nOtherwise the initialization will fail.')
         self.address = str(serial_no)
+        self.verbose = verbose
+        self.velocity = velocity
 
     def initialize_instrument(self):
         DeviceManagerCLI.BuildDeviceList()
@@ -43,41 +47,133 @@ class K10CR1_stage:
 
         # Get Device Information and display description.
         self.device_info = self.device.GetDeviceInfo()
-        print(self.device_info.Description)
+        if self.verbose:
+            print(self.device_info.Description)
         # Load any configuration settings needed by the controller/stage.
         self.device.LoadMotorConfiguration(self.address, DeviceConfiguration.DeviceSettingsUseOptionType.UseDeviceSettings)
         motor_config = self.device.LoadMotorConfiguration(self.address)
 
+        # self.device.SetVelocityParams(self.device.GetVelocityParams())
+        self.VelocityParams = self.device.GetVelocityParams()
+        self.VelocityParams.MaxVelocity = Decimal(self.velocity)
+        self.device.SetVelocityParams(self.VelocityParams)
+
+
     def home_device(self, timeout=60000):
         # Call device methods.
-        # print("Homing the rotation stage of SN:  " + self.address)
+        if self.verbose:
+            print("Homing the rotation stage of SN:  " + self.address)
         self.device.Home(timeout)  # 60 second timeout.
-        # print("The system is at home position. This position will be referred as 0 degree.")
+        if self.verbose:
+            print("The system is at home position. This position will be referred as 0 degree.")
 
     def move(self, angle=0):
+
         while True:
             if angle < 0:
-                angle += 360
+                angle = angle + 360.0
             else:
                 break
         while True:
-            if angle > 360:
-                angle -= 360
+            if angle > 360.0:
+                angle = angle - 360.0
             else:
                 break
         new_pos = Decimal(angle)  # Must be a .NET decimal.
-        # print("Moving the rotation stage of SN: " + self.address + f' to {new_pos} degrees')
+        if self.verbose:
+            print("Moving the rotation stage of SN: " + self.address + f' to {new_pos} degrees')
         self.device.MoveTo(new_pos, 60000)  # 60 second timeout.
-        # print("Movement finished.")
+        if self.verbose:
+            print("Movement finished.")
+
+    def read(self):
+        return Decimal.ToDouble(self.device.Position)
 
     def quit(self):
-        # self.home_device()
+        self.home_device()
         # Stop polling loop and disconnect device before program finishes. 
         self.device.StopPolling()
         self.device.Disconnect()
-        # print("Disconnectd the rotation stage of SN: " + self.address)
+        if self.verbose:
+            print("Disconnectd the rotation stage of SN: " + self.address)
+
+
+# class K10CR1_stage:
+#     def __init__(self, serial_no=55425494) -> None:
+#         # print('Important: make sure you are not running Kinesis software in the meantime. \nOtherwise the initialization will fail.')
+#         self.address = str(serial_no)
+
+#     def initialize_instrument(self):
+#         DeviceManagerCLI.BuildDeviceList()
+#         self.device = CageRotator.CreateCageRotator(self.address)
+#         self.device.Connect(self.address)
+
+#         if not self.device.IsSettingsInitialized():
+#             self.device.WaitForSettingsInitialized(10000)  # 10 second timeout.
+#             assert self.device.IsSettingsInitialized() is True
+        
+#         # Start polling loop and enable device.
+#         self.device.StartPolling(250)  #250ms polling rate.
+#         time.sleep(2)
+#         self.device.EnableDevice()
+#         time.sleep(0.25)  # Wait for device to enable.
+
+#         # Get Device Information and display description.
+#         self.device_info = self.device.GetDeviceInfo()
+#         print(self.device_info.Description)
+#         # Load any configuration settings needed by the controller/stage.
+#         self.device.LoadMotorConfiguration(self.address, DeviceConfiguration.DeviceSettingsUseOptionType.UseDeviceSettings)
+#         motor_config = self.device.LoadMotorConfiguration(self.address)
+
+#     def home_device(self, timeout=60000):
+#         # Call device methods.
+#         # print("Homing the rotation stage of SN:  " + self.address)
+#         self.device.Home(timeout)  # 60 second timeout.
+#         # print("The system is at home position. This position will be referred as 0 degree.")
+
+#     def move(self, angle=0):
+#         while True:
+#             if angle < 0:
+#                 angle += 360
+#             else:
+#                 break
+#         while True:
+#             if angle > 360:
+#                 angle -= 360
+#             else:
+#                 break
+#         new_pos = Decimal(angle)  # Must be a .NET decimal.
+#         # print("Moving the rotation stage of SN: " + self.address + f' to {new_pos} degrees')
+#         self.device.MoveTo(new_pos, 60000)  # 60 second timeout.
+#         # print("Movement finished.")
+
+#     def quit(self):
+#         # self.home_device()
+#         # Stop polling loop and disconnect device before program finishes. 
+#         self.device.StopPolling()
+#         self.device.Disconnect()
+#         # print("Disconnectd the rotation stage of SN: " + self.address)
 
     
+class PowerMeter:
+    def __init__(self, address='USB0::0x1313::0x8078::P0011410::INSTR'):
+        self.rm = pyvisa.ResourceManager()
+        self.pm = self.rm.open_resource('USB0::0x1313::0x8078::P0011410::INSTR')
+        self.pm.write("SENS:RANGE:AUTO ON")
+        self.pm.write("SENS:CORR:WAV 658")
+        #set units to Watts
+        self.pm.write("SENS:POW:UNIT W")
+        #set averaging to 1000 points
+        self.pm.write("SENS:AVER:1000")
+
+    def measure(self):
+        power = self.pm.query_ascii_values("MEAS:POW?")[0]
+        return power
+    
+    def close(self):
+        self.pm.close()
+        self.rm.close()
+
 
 def main():
     """The main entry point for the application"""
