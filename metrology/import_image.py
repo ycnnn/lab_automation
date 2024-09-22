92015e29cbb75e6a6e7b6d31a3c2b06c1af30f71
Yue Zhang <yuez11@illinois.edu>
2024-09-22 14:09:23 -0500

    .

--- metrology/import_image.py
+++ metrology/import_image.py
@@ -20,8 +20,7 @@ def setup_image(cv, image_path, magification_text_identifier='100x'):
     mag = 100 if magification_text_identifier in image_path else 10
     micron_conversion_factor = 0.29072 * 10 / mag
     
-    # transform_params = np.load(img_path.split('.')[0] + '.npy')
-    with open(img_path.split('.')[0] + '.json', 'r') as file:
+    with open(image_path.split('.')[0] + '.json', 'r') as file:
         transform_params = json.load(file)
     x_center, y_center, rotation = transform_params
     
@@ -36,23 +35,16 @@ layout_name = "L0LONG.gds"
 app, main_window, cv = open_layout(layout_name)
 
 
-img_path = 'clean.jpg'
-setup_image(cv=cv, 
-            image_path=img_path, )
 
+setup_image(cv=cv, 
+            image_path='clean.jpg')
+
+setup_image(cv=cv, 
+            image_path='noisy.jpg')
             
-# img_path = 'noisy.jpg'
-# setup_image(cv=cv, 
-#             image_path=img_path, 
-#             rotation=1.29686366, 
-#             x_center=-927.38361082, y_center=12884.86185968)
-            
-# img_path = '100x.jpg'
-# setup_image(cv=cv, 
-#             image_path=img_path, 
-#             rotation= 2.6630008220672607, 
-#             x_center=-5550.869002235031, y_center=-22243.621066253218,
-#             micron_conversion_factor=0.02907)
+
+setup_image(cv=cv, 
+            image_path='100x.jpg')
 
 
 
