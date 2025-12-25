dbddbec890d69b79d4b02dc2a21e912b5ebe1728
Yue Zhang <yueh.cheung@outlook.com>
2025-12-25 12:08:51 -0800

    .

--- plot_helper/setup.py
+++ plot_helper/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='plot_helper',
-        version='4.0',
+        version='5.0',
         description='Collection of helper functions for a beautiful scientific plots made from matplotlib',
         author='ycnnn',
         license='MIT',
